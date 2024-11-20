5d30f23ee53afd7aeb5d0ebf09336de3aa28c6c0
tatyanepgoncalves <tatyane.2324165@aluno.faculdadedescomplica.com.br>
2024-11-20 20:25:23 -0300

    🔧 chore: Configuração da página

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-
+import { useState } from "react";
+import Modal from "./components/Modal/Modal";
+import RatingCard from "./components/RatingCard/RatingCard";
+import "./styles/global.scss";
 
 export default function App() {
+  const [rating, setRating] = useState(null);
+
   return (
-    <div>App</div>
-  )
+    <main className="app">
+      {!rating ? (
+        <RatingCard onSubmit={setRating} />
+      ): (
+
+        <Modal rating={rating} />
+      )}
+    </main>
+  );
 }
